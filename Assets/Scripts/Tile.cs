4cddbf8cd4b2f684e6ef2b620c6bf5c4cce51a76
noooooooooooooooob <168753086+noooooooooooooooob@users.noreply.github.com>
2025-11-13 01:06:45 +0900

    rigging and shadow complete

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
-    
+    public Vector2Int tileOffset; // Offset of the tile in the grid
+    Vector2Int gridPosition;
+    public Vector2Int GridPosition
+    {
+        get { return gridPosition; }
+        set { gridPosition = value; }
+    }
+
 }
